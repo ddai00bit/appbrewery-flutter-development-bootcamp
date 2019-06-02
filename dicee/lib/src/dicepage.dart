9e6079e5f66359f492e6f92c81c3e4ec09b3461c
lessercodes <lessercodes@gmail.com>
2019-06-02 17:40:21 +0200

    [dicee] [Add two dice images to the layout]

--- dicee/lib/src/dicepage.dart
+++ dicee/lib/src/dicepage.dart
@@ -3,6 +3,19 @@ import 'package:flutter/material.dart';
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Image(
+            image: AssetImage("images/dice1.png"),
+          ),
+        ),
+        Expanded(
+          child: Image(
+            image: AssetImage("images/dice1.png"),
+          ),
+        ),
+      ],
+    );
   }
 }
