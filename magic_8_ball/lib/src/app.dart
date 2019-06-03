582232ccc5aedccc490b2eade06550df9fb4462c
lessercodes <lessercodes@gmail.com>
2019-06-03 19:13:01 +0200

    [magic_8_ball] [Add basic layout]

---
+++ magic_8_ball/lib/src/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'ball_page.dart';
+
+class Magic8BallApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BallPage(),
+    );
+  }
+}
---
+++ magic_8_ball/lib/src/ball_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Container(),
+    );
+  }
+}
