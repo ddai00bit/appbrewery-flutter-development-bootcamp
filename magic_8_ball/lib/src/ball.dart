cf9cc918367f3e42bd6d29572cb9037376dd2dc5
lessercodes <lessercodes@gmail.com>
2019-06-03 19:16:56 +0200

    [magic_8_ball] [Display static Magic 8 Ball]

---
+++ magic_8_ball/lib/src/ball.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset('images/ball1.png'),
+    );
+  }
+}
--- magic_8_ball/lib/src/ball_page.dart
+++ magic_8_ball/lib/src/ball_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'ball.dart';
+
 class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -9,7 +11,7 @@ class BallPage extends StatelessWidget {
         title: Text('Ask Me Anything'),
         backgroundColor: Colors.blue[900],
       ),
-      body: Container(),
+      body: Ball(),
     );
   }
 }
