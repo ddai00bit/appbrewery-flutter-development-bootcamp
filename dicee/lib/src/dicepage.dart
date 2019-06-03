dd6c4408880a2fb6038bf01f4d060819ea628429
lessercodes <lessercodes@gmail.com>
2019-06-03 07:11:02 +0200

    [dicee] [Add roll dice logic]

--- dicee/lib/src/dicepage.dart
+++ dicee/lib/src/dicepage.dart
@@ -1,6 +1,24 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDice = 1;
+  int rightDice = 1;
+
+  void _rollDice() {
+    var rand = Random();
+    setState(() {
+      leftDice = rand.nextInt(6) + 1;
+      rightDice = rand.nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -10,16 +28,18 @@ class DicePage extends StatelessWidget {
             child: FlatButton(
               onPressed: () {
                 print('Left button got pressed');
+                _rollDice();
               },
-              child: Image.asset("images/dice1.png"),
+              child: Image.asset("images/dice$leftDice.png"),
             ),
           ),
           Expanded(
             child: FlatButton(
               onPressed: () {
                 print('Right button got pressed');
+                _rollDice();
               },
-              child: Image.asset("images/dice1.png"),
+              child: Image.asset("images/dice$rightDice.png"),
             ),
           ),
         ],
