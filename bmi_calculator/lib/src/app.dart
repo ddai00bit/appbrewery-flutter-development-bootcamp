0bf39e8c69869316936a44229a34426bf76ddb7f
lessercodes <lessercodes@gmail.com>
2019-06-17 21:04:32 +0200

    [bmi_calculator] [Initialize bmi_calculator project]

---
+++ bmi_calculator/lib/src/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'input_page.dart';
+
+class BMICalculatorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: InputPage(),
+    );
+  }
+}
