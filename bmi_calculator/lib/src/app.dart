9b1b88778a5d6ecbe8c062e83237f5217c57452a
Csaba Szabo <csaba.szabo@oce.com>
2019-06-18 12:38:28 +0200

    [bmi_calculator] [Set basic theme colors]

--- bmi_calculator/lib/src/app.dart
+++ bmi_calculator/lib/src/app.dart
@@ -6,6 +6,10 @@ class BMICalculatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E22),
+        scaffoldBackgroundColor: Color(0xFF0A0E22),
+      ),
       home: InputPage(),
     );
   }
