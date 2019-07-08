6898dc058533fb30b0e8c2fe01be5029b5479aee
lessercodes <lessercodes@gmail.com>
2019-07-08 22:05:32 +0200

    [clima] [Create base App widget]

---
+++ clima/lib/src/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ClimaApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: Container(),
+    );
+  }
+}
