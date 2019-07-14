1c35dc954d5cd7d1c2ce1b885787f556f0ee63f3
lessercodes <lessercodes@gmail.com>
2019-07-14 11:22:44 +0200

    [clima] [Handle exceptions]

--- clima/lib/src/screens/loading_screen.dart
+++ clima/lib/src/screens/loading_screen.dart
@@ -15,8 +15,12 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocation() async {
-    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-    print(position);
+    try {
+      Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+      print(position);
+    } catch (e) {
+      print(e);
+    }
   }
 
   @override
