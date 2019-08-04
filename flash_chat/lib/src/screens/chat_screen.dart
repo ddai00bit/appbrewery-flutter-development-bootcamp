c4f9ade6bf6cce9e93faedb9da93cf74e590a54f
lessercodes <lessercodes@gmail.com>
2019-08-04 12:25:11 +0200

    [flash_chat] [Create ChatScreen]

--- flash_chat/lib/src/screens/chat_screen.dart
+++ flash_chat/lib/src/screens/chat_screen.dart
@@ -1,12 +1,58 @@
 import 'package:flutter/material.dart';
 
+import '../constants.dart';
+
 class ChatScreen extends StatelessWidget {
   static const String ID = "chat_screen";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Chat'),
+      appBar: AppBar(
+        leading: null,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.close),
+              onPressed: () {
+                //Implement logout functionality
+              }),
+        ],
+        title: Text('⚡️Chat'),
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              decoration: MessageContainerDecoration,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      onChanged: (value) {
+                        //Do something with the user input.
+                      },
+                      decoration: MessageTextFieldDecoration,
+                    ),
+                  ),
+                  FlatButton(
+                    onPressed: () {
+                      //Implement send functionality.
+                    },
+                    child: Text(
+                      'Send',
+                      style: SendButtonTextStyle,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
